9ad70e505b88a07d401aceb9f982350d20a9c3d8
Bhavana <bhavana@MACs-MacBook-Pro.local>
2020-06-27 16:39:03 +0400

    myfile

---
+++ src/add/add.java
@@ -0,0 +1,14 @@
+package add;
+
+public class add {
+
+		// TODO Auto-generated constructor stub
+		public static void main(String[] args) {
+			
+			int num1=10,num2=20,sum;
+			sum=num1+num2;
+			System.out.println("sum of two integer is:"+sum);
+		}
+	}
+
+
---
+++ src/add/forloop.java
@@ -0,0 +1,10 @@
+package add;
+
+public class forloop {
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 5; i++) {
+			System.out.printf("The value of i is: %d\n", i);
+		}
+	}
+}
