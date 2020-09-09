efab891970cfd0c5cc6a9babc2cbd22b30e5cbbd
bhavanapatidar <67507369+bhavanapatidar@users.noreply.github.com>
2020-09-09 16:41:31 +0400

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,12 @@
+
+
+public class addition {
+
+   public static void main(string[] args){
+	   
+	   int num1=10,num2=15,sum;
+	   sum=num1+num2;
+	   System.out.println("sum of these numbers:"+sum);
+   }
+
+}
---
+++ valuehello.java
@@ -0,0 +1,16 @@
+package value;
+
+public class valuehello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int value=0;
+      while(value<10)
+      {
+    	  System.out.println("hello"+value);
+    	  value++;
+    	  
+      }
+	}
+
+}
